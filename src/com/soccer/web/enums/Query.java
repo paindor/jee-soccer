2ba03b55aac67b883d6b97bff1389c8ecf4cb258
최광진 <paindorrr@gmail.com>
2019-09-26 19:34:43 +0900

    0926 final

--- src/com/soccer/web/enums/Query.java
+++ src/com/soccer/web/enums/Query.java
@@ -1,8 +1,45 @@
 package com.soccer.web.enums;
 
 public enum Query {
-	SELECT_BY_VALUE1,
-	SELECT_BY_VALUE2
+	SELECT1_BY_VALUE1,
+	SELECT1_BY_VALUE2,
+	SELECT2_BY_VALUE1,
+	SELECT2_BY_VALUE2;
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		
+		String query = "";
+		
+		switch(this) {
+		case SELECT1_BY_VALUE1:
+			query = "SELECT ? \r\n" + 
+					"FROM ?\r\n" + 
+					"WHERE ? LIKE ?";
+			break;
+		case SELECT1_BY_VALUE2:
+			query = "SELECT ? \r\n" + 
+					"FROM ?\r\n" + 
+					"WHERE ? LIKE ? AND ? LIKE ?";
+			break;
+		case SELECT2_BY_VALUE1:
+			query = "SELECT ? ? \r\n" + 
+					"FROM ?\r\n" + 
+					"WHERE ? LIKE ?";
+			break;
+		case SELECT2_BY_VALUE2:
+			query = "SELECT ? ? \r\n" + 
+					"FROM ?\r\n" + 
+					"WHERE ? LIKE ? AND ? LIKE ?";
+			break;
+			
+			
+		}
+		return query;
+	}
 	
 
 }
